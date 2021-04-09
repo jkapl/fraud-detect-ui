856e7875a97ec7b77654ac7c5d3668ceafeaa73f
Joel Kaplan <joelkaplan1@gmail.com>
2021-04-09 09:40:48 -0400

    Messages API updates

--- src/api/messagesAPI.js
+++ src/api/messagesAPI.js
@@ -1,4 +1,25 @@
 export async function getMessages () {
-    const res = await fetch('/api/messages')
-    return res.json();
+    let res = {
+        "transaction_id" : "uuid",
+        "device_metadata": {
+          "device_id": "cy56ayzpdp",
+          "location": "94016",
+          "merchant_name": "chase"
+        },
+        "transaction": {
+          "device_id": "cy56ayzpdp",
+          "transaction_value": "1000",
+          "account_id": "8mmvrnxiuy",
+          "time_ms": "1593617082456",
+          "flagged": false
+        }
+    }
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(JSON.stringify(res))
+        }, 2000);
+    });
+
+    // const res = await fetch('/api/messages')
+    // return res.json();
 }
